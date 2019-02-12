92023eed4eb57a0a02dab9177d04fb9ad2f0d2aa
dschell97 <47043248+dschell97@users.noreply.github.com>
2019-02-11 19:55:51 -0500

    Add files via upload

---
+++ fixed.cc
@@ -0,0 +1,47 @@
+/*
+	Daniel Schell
+	ds008016@ohio.edu
+
+	description: declares a vector and calc stdev  for the set
+
+	11 February 2019
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+double deviation(int* a, int n )
+{
+	//declares a variable sum
+	int sum = 0;
+
+	//for loop to calc sum
+	for(size_t i = 0; i <= n - 1; i++)
+	{
+		sum += a[i];
+	}
+
+	//calcs the mean of the set and declares it as a variable
+	double mean = sum/=(n-1);
+	//declares stdev variable 
+	double stdev = 0;
+
+	//for loop to calc the std root value
+	for(size_t i = 0; i <= n - 2; i++)
+	{
+		stdev += (a[i] - mean) * (a[i] - mean);
+	}
+	//stdev is divided by number of numbers in array
+	stdev /= (n-1);
+
+	//fixes the standard deviation is zero error
+	if(stdev == 0)
+	{
+		std::cout << "Sigma is zero." << std::endl;
+	}
+
+	//return root of the stdev to give variance
+	return sqrt(stdev);
+
+}
